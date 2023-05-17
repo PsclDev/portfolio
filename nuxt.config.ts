429df98f0e298638b4cbe0fc7b1dbc4466808596
PsclDev <me@pscl.dev>
2023-05-17 23:08:24 +0200

    feat: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,27 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'PsclDev - Developer',
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '~/public/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '~/public/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '~/public/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '~/public/site.webmanifest' },
+        { rel: 'mask-icon', href: '~/public/safari-pinned-tab.svg' },
+      ],
     },
   },
   css: ['~/fonts/css/kill-the-noise.css', '~/assets/styles/main.css'],
